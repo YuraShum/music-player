f2ef1de928778d2a0f680c38a07c85583656f6cc
YuraShum <91155294+YuraShum@users.noreply.github.com>
2024-09-02 14:16:30 +0300

    created response status handlers

---
+++ server/src/handlers/response.js
@@ -0,0 +1,70 @@
+import { response } from "express"
+
+
+const responseConstuctor = (response, statusCode, data) => {
+    return response.status(statusCode).json(data)
+}
+
+
+const responseHendlers = {
+    unautorize: (response) => {
+        return responseConstuctor(
+            response,
+            401,
+            {
+                message: 'Немає доступу. Потрібна авторизація.',
+                status: 401
+            }
+        )
+    },
+    forbidden: (response) => {
+        return responseConstuctor(
+            response,
+            403,
+            {
+                message: 'Неправильний або прострочений токен',
+                status: 403
+            }
+        )
+    },
+    badRequest: (response, message) => {
+        return responseConstuctor(
+            response,
+            400,
+            {
+                message,
+                status: 400
+            }
+        )
+    },
+    created: (response, data) => {
+        return responseConstuctor(response, 201, data)
+
+    },
+    error: (response) => {
+        return responseConstuctor(
+            response,
+            500,
+            {
+                message: 'Щось пішло не так',
+                status: 500
+            }
+        )
+    },
+    notFound: (response, message) => {
+        return responseConstuctor(
+            response,
+            404,
+            {
+                message,
+                status: 404
+            }
+        )
+    },
+    ok: (response, data) => {
+        return responseConstuctor(response, 200, data)
+    }
+
+}
+
+export default responseHendlers
