b697603bd261a388fe4429d39bfe21cde3d5187e
YuraShum <91155294+YuraShum@users.noreply.github.com>
2024-09-05 14:04:12 +0300

    created nav bar section for moving in the app

---
+++ client/src/const/navbarConfig.ts
@@ -0,0 +1,43 @@
+
+import { IconType } from "react-icons";
+import { SiNeteasecloudmusic } from "react-icons/si";
+import { RiHome2Fill } from "react-icons/ri";
+import { IoMusicalNotes } from "react-icons/io5";
+import { RiPlayListFill } from "react-icons/ri";
+import { GrFavorite } from "react-icons/gr";
+import { VscAccount } from "react-icons/vsc";
+
+
+interface NavigateItem {
+    link: string;
+    icon: IconType;
+}
+
+const NAV_BAR_CONFIG: NavigateItem[] = [
+    {
+        icon: SiNeteasecloudmusic,
+        link: '/',
+    },
+    {
+        icon: RiHome2Fill,
+        link: '/home',
+    },
+    {
+        icon: IoMusicalNotes,
+        link: '/songs',
+    },
+    {
+        icon: RiPlayListFill,
+        link: '/playlists',
+    },
+    {
+        icon: GrFavorite,
+        link: '/favorites',
+    },
+    {
+        icon: VscAccount,
+        link: '/account',
+    },
+]
+
+export default NAV_BAR_CONFIG
