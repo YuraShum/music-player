76af1d4a59bcfc31c2feaad0483ecc5dc4549f62
YuraShum <91155294+YuraShum@users.noreply.github.com>
2024-09-09 22:23:48 +0300

    change cards style

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -8,18 +8,16 @@ import { useEffect, useState } from "react";
 import userApi from "@/api/requests/user.requests";
 import Song from "@/components/ui/Song";
 import Playlist from "@/components/ui/Playlist";
+
 export default function Home() {
   const { user } = useSelector((state: any) => state.user)
 
-  //!! оновлення get userInformation
   const [data, setData] = useState({})
   const [userRating, setUserRating] = useState(null)
 
   useEffect(() => {
-    // Функція для отримання даних про рейтинг користувача та інформацію про користувача
     const fetchData = async () => {
       try {
-        // Виконуємо запити одночасно через Promise.all
         const [{ response: ratingResponse, error: ratingError }, { response: infoResponse, error: infoError }] = await Promise.all([
           userApi.getUserRating(),
           userApi.getUserInformation(),
@@ -43,40 +41,62 @@ export default function Home() {
       }
     };
 
-    // Викликаємо функцію отримання даних
     fetchData();
   }, []);
+
   return (
     <div className="rounded-3xl h-full p-6">
-      {/** information section */}
       <h1 className="text-center text-4xl mt-3">Statistic</h1>
       <div className="flex gap-8 justify-center mt-16">
-        <div className="bg-gray-200 w-fit p-6 rounded-lg shadow-xl flex flex-col items-center gap-4 ">
-          <h2>Downloaded tracks</h2>
-          <div className=" w-40 h-40 bg-orange-400 border-[10px] border-orange-500 rounded-full flex justify-center items-center text-6xl font-bold text-white">
-            {data?.songs?.length}
-          </div>
-        </div>
-        <div className="bg-gray-200 w-fit p-6 rounded-lg shadow-xl flex flex-col items-center gap-4 scale-125">
-          <h2>user rating</h2>
-          <div className=" w-40 h-40 bg-yellow-400 border-[10px] border-yellow-500 rounded-full flex justify-center items-center text-6xl font-bold text-white">
-            <div className="relative">
-              <span>{userRating?.rank}</span>
-              <span>/</span>
-              <span>{userRating?.totalUsers}</span>
+        <div className="p-[4px] relative">
+          <div className="absolute inset-0 bg-gradient-to-r from-orange-300 to-orange-600 rounded-lg" />
+          <div className="bg-transparent rounded-[6px] relative group transition duration-200 text-white hover:bg-transparent">
+            <div className="bg-gray-200 w-fit p-6 rounded-lg shadow-xl flex flex-col items-center gap-4">
+              <h2 className="text-xl font-semibold text-orange-600 tracking-wide">
+                Downloaded tracks
+              </h2>
+              <div className="w-40 h-40 bg-orange-400 border-[10px] border-orange-500 rounded-full flex justify-center items-center text-6xl font-bold text-white">
+                {data?.songs?.length}
+              </div>
             </div>
           </div>
         </div>
-        <div className="bg-gray-200 w-fit p-6 rounded-lg shadow-xl flex flex-col items-center gap-4">
-          <h2>created playlists</h2>
-          <div className=" w-40 h-40 bg-cyan-400 border-[10px] border-cyan-500 rounded-full flex justify-center items-center text-6xl font-bold text-white">
-            {data?.playlists?.length}
+
+        <div className="p-[4px] relative scale-125">
+          <div className="absolute inset-0 bg-gradient-to-r from-yellow-300 to-yellow-600 rounded-lg" />
+          <div className="bg-transparent rounded-[6px] relative group transition duration-200 text-white hover:bg-transparent">
+            <div className="bg-gray-200 w-fit p-6 rounded-lg shadow-xl flex flex-col items-center gap-4">
+              <h2 className="text-xl font-semibold text-yellow-600 tracking-wide ">
+                User rating
+              </h2>
+              <div className="w-40 h-40 bg-yellow-400 border-[10px] border-yellow-500 rounded-full flex justify-center items-center text-6xl font-bold text-white">
+                <div className="relative">
+                  <span>{userRating?.rank}</span>
+                  <span>/</span>
+                  <span>{userRating?.totalUsers}</span>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
 
+        <div className="p-[4px] relative">
+          <div className="absolute inset-0 bg-gradient-to-r from-cyan-300 to-cyan-600 rounded-lg" />
+          <div className="bg-transparent rounded-[6px] relative group transition duration-200 text-white hover:bg-transparent">
+            <div className="bg-gray-200 w-fit p-6 rounded-lg shadow-xl flex flex-col items-center gap-4">
+              <h2 className="text-xl font-semibold text-cyan-600 tracking-wide">
+                Created playlists
+              </h2>
+              <div className="w-40 h-40 bg-cyan-400 border-[10px] border-cyan-500 rounded-full flex justify-center items-center text-6xl font-bold text-white">
+                {data?.playlists?.length}
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
+
       <div className="flex flex-col">
-        {/** download music section */}
+        {/** Download music section */}
         <div className="mt-16">
           <h2 className="text-2xl font-bold">Recently downloaded songs</h2>
           <div className="flex flex-col gap-6 p-6">
@@ -88,19 +108,21 @@ export default function Home() {
                 mp3={song.mp3}
                 cover={song.cover}
                 index={index}
-                id={song._id} />
+                id={song._id}
+              />
             ))}
           </div>
         </div>
-        {/** Created playlist */}
+
+        {/** Created playlists */}
         <div className="mt-16">
           <h2 className="text-2xl font-bold">Recently created playlists</h2>
           <div className="flex flex-col gap-6 p-6">
             {data?.playlists?.map((playlist, index) => (
               <Playlist
-              key={playlist._id}
+                key={playlist._id}
                 name={playlist.name}
-                description = {playlist.description}
+                description={playlist.description}
                 songs={playlist.songs}
                 id={playlist._id}
               />
@@ -108,7 +130,6 @@ export default function Home() {
           </div>
         </div>
       </div>
-
     </div>
   );
 }
