d2a39c0c6c0f98b541deadfe904f0ae1fe4602d1
YuraShum <91155294+YuraShum@users.noreply.github.com>
2024-09-27 13:53:51 +0300

    added toast container

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <ClientProvider>
-        <ToastContainer />
+        <ToastContainer  className='z-20'/>
           {children}
         </ClientProvider>
       </body>
